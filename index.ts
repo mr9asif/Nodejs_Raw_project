f5e9c4cb5413df17de66c18b5040878b6b3f0683
mr9 asif <asifalibd002@gmail.com>
2026-05-19 22:37:18 +0600

    project setup

---
+++ index.ts
@@ -0,0 +1,9 @@
+import http from "http";
+
+const server = http.createServer((req, res)=>{
+    res.end("hello from server");
+})
+
+server.listen(3000, ()=>{
+    console.log("server running on 3000")
+})
